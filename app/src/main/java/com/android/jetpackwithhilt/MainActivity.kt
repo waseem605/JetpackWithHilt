4e15f52e325d648988b1ac1d2b9050f93ea3f0ec
Waseem Shahzad <85110872+waseem605@users.noreply.github.com>
2022-11-14 12:39:24 +0500

    Notification Android 13

--- app/src/main/java/com/android/jetpackwithhilt/MainActivity.kt
+++ app/src/main/java/com/android/jetpackwithhilt/MainActivity.kt
@@ -10,11 +10,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.app.NotificationManager
+import android.content.Context
+import androidx.compose.material.Button
+import android.os.Build
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.android.jetpackwithhilt.searchImage.components.MainComponent
 import com.android.jetpackwithhilt.ui.theme.JetpackWithHiltTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +36,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackWithHiltTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -58,14 +70,56 @@ fun MyApp(content:@Composable ()->Unit) {
 @Composable
 fun WallpaperScreens() {
     Column(modifier = Modifier.fillMaxSize()) {
+        val context = LocalContext.current
+        var hasNotificationPermission by remember {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S_V2) {
+                mutableStateOf(
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) == PackageManager.PERMISSION_GRANTED
+                )
+            } else mutableStateOf(true)
+        }
         Column(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = { isGranted ->
+                    hasNotificationPermission = isGranted
+                }
+            )
+            Button(onClick = {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }) {
+                Text(text = "Request permission")
+            }
+            Button(onClick = {
+                if(hasNotificationPermission) {
+                    showNotification(context)
+                }
+            }) {
+                Text(text = "Show notification")
+            }
             MyApp {
                 MainComponent()
             }
         }
+
     }
+}
+
+private fun showNotification(context: Context) {
+    val notification = NotificationCompat.Builder(context, "channel_id")
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle("Hello world")
+        .setContentText("This is a description")
+        .build()
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.notify(1, notification)
 }
--- app/src/main/java/com/android/jetpackwithhilt/searchImage/BaseApplication.kt
+++ app/src/main/java/com/android/jetpackwithhilt/searchImage/BaseApplication.kt
@@ -1,8 +1,24 @@
 package com.android.jetpackwithhilt.searchImage
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "channel_id",
+                "Channel name",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
 }
